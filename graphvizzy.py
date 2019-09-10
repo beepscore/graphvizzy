1c4ec7c907e83c126a5b8016f9bb336ece890a56
beepscore <support@beepscore.com>
2019-09-09 21:41:20 -0700

    add graphvizzy.py

---
+++ graphvizzy.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import pygraphviz as pgv
+
+
+def draw_graph(out_file_name):
+    """
+    """
+
+    # init empty graph
+    A = pgv.AGraph()
+
+    # set some default node attributes
+    A.node_attr['style']='filled'
+    A.node_attr['shape']='circle'
+
+    # Add edges (and nodes)
+    A.add_edge(1,2)
+    A.add_edge(2,3)
+    A.add_edge(1,3)
+
+    # layout with default (neato)
+    A.layout()
+
+    # draw png
+    png = A.draw(format='png')
+
+    # print dot file to standard output
+    # print(A.string())
+
+    # write
+    with open(out_file_name, 'w') as f_out:
+        f_out.write(A.string())
+
+def draw_graph2(out_file_name):
+    """
+    https://github.com/pgv/pygraphviz/blob/master/examples/subgraph.py
+    """
+
+    # init empty graph
+    A = pgv.AGraph()
+
+    # add some edges
+    A.add_edge(1,2)
+    A.add_edge(2,3)
+    A.add_edge(1,3)
+    A.add_edge(3,4)
+    A.add_edge(3,5)
+    A.add_edge(3,6)
+    A.add_edge(4,6)
+
+    # make a subgraph with rank='same'
+    B = A.add_subgraph([4,5,6],name='s1',rank='same')
+    B.graph_attr['rank']='same'
+
+    # write
+    with open(out_file_name, 'w') as f_out:
+        # write A not B
+        f_out.write(A.string())
+
+
+if __name__ == '__main__':
+
+    # draw_graph('./data/g.dot')
+    draw_graph2('./data/g2.dot')
