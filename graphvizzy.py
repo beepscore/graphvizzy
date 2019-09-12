aa27ade91c333aa653ed21e82c610730f658ddd5
beepscore <support@beepscore.com>
2019-09-11 19:05:25 -0700

    delete draw_graph2 and rename draw_graph3 to draw_graph2.

--- graphvizzy.py
+++ graphvizzy.py
@@ -33,35 +33,10 @@ def draw_graph(out_file_name):
     with open(out_file_name, 'w') as f_out:
         f_out.write(A.string())
 
+
 def draw_graph2(out_file_name):
     """
     https://github.com/pgv/pygraphviz/blob/master/examples/subgraph.py
-    """
-
-    # init empty graph
-    A = pgv.AGraph()
-
-    # add some edges
-    A.add_edge(1,2)
-    A.add_edge(2,3)
-    A.add_edge(1,3)
-    A.add_edge(3,4)
-    A.add_edge(3,5)
-    A.add_edge(3,6)
-    A.add_edge(4,6)
-
-    # make a subgraph with rank='same'
-    B = A.add_subgraph([4,5,6],name='s1',rank='same')
-    B.graph_attr['rank']='same'
-
-    # write
-    with open(out_file_name, 'w') as f_out:
-        # write A not B
-        f_out.write(A.string())
-
-
-def draw_graph3(out_file_name):
-    """
     """
 
     # init empty graph
@@ -93,5 +68,4 @@ def draw_graph3(out_file_name):
 if __name__ == '__main__':
 
     # draw_graph('./data/g.dot')
-    # draw_graph2('./data/g2.dot')
-    draw_graph3('./data/g3.dot')
+    draw_graph2('./data/g2.dot')
